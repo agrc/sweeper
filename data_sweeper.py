fe23960dd00043f3825a7a855ffeed189f4bf27c
Greg Bunce <gbunce@utah.gov>
2019-07-23 15:04:24 -0600

    initial commit

---
+++ data_sweeper.py
@@ -0,0 +1,6 @@
+# import sweep_duplicates, sweep_empty_geometry, sweep_invalid_geometry, delete_row, log_to_text
+from doctopt import docopt
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
