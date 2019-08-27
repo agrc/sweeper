a11b941c53ac6dfa8b3d1e022dad8fa33f30ed6d
steveoh <sgourley@utah.gov>
2019-08-27 09:25:23 -0600

    refactor: add sweeper base class
    refs #5

---
+++ src/sweeper/sweepers/sweeperbase.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+'''
+sweeperbase.py
+A class that defines the sweeper.sweepers contract
+'''
+
+class SweeperBase(object):
+  def __init__(self):
+    pass
+
+  def sweep(self, params):
+    raise NotImplemented()
+
+  def get_report(self, params):
+    raise NotImplemented()
+
+  def try_fix(self, params):
+    raise NotImplemented()
