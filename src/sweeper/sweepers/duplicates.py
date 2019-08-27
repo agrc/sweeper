9d3f66e400bb4b34abba9e947a01fe445108421e
Zach Beck <zbeck@utah.gov>
2019-08-27 16:13:52 -0600

    I need help (#9)

--- src/sweeper/sweepers/duplicates.py
+++ src/sweeper/sweepers/duplicates.py
@@ -9,54 +9,50 @@ import arcpy
 import re
 from xxhash import xxh64
 
-def removeNone(word):
-    if word == None:
-        word = ''
-    return word
 
-def deleteDuplicateFeatures(inFc):
+class DuplicateTest(object):
+    def __init__(self):
 
-    digestDict = {}
-    duplicateLst = []
-    dupePrintLst = []
-    digTrim = re.compile(r'(\d+\.\d{2})(\d+)')
 
-    shps = ['Shape', 'SHAPE']
+    def sweep(self, fc):
 
-    fields = [f.name for f in arcpy.ListFields(inFc) if f.name not in shps]
-    fields.append('SHAPE@WKT')
-    print(fields)
-    # if 'Shape' in fields:
-    #     shpIndex = fields.index('Shape')
-    # else:
-    #     shpIndex = fields.index('SHAPE')
+        digestDict = {}
+        duplicateDict = {}
+        dupePrintLst = []
+        digTrim = re.compile(r'(\d+\.\d{2})(\d+)')
 
-    with arcpy.da.SearchCursor(inFc, fields) as sCursor:
-        for row in sCursor:
-            shp = row[-1]
-            if shp != None:
-                coordTrim = digTrim.sub(r'\1', shp)
-                hash = xxh64('{} {}'.format(row[1:], coordTrim))
-                digest = hash.hexdigest()
-                if digest not in digestDict:
-                    digestDict.setdefault(digest)
+        shps = ['Shape', 'SHAPE']
+
+        fields = [f.name for f in arcpy.ListFields(fc) if f.name not in shps]
+        fields.append('SHAPE@WKT')
+
+        with arcpy.da.SearchCursor(fc, fields) as sCursor:
+            for row in sCursor:
+                shp = row[-1]
+                if shp != None:
+                    coordTrim = digTrim.sub(r'\1', shp)
+                    hash = xxh64('{} {}'.format(row[1:], coordTrim))
+                    digest = hash.hexdigest()
+                    if digest not in digestDict:
+                        digestDict.setdefault(digest)
+                    else:
+                        duplicates = duplicateDict.setdefault('Duplicate Features', [])
+                        duplicates.extend(row[0])
                 else:
-                    duplicateLst.append(row[0])
-                    dupePrintLst.append(row)
-            if shp == None:
-                duplicateLst.append(row[0])
-                dupePrintLst.append(row)
+                    duplicateDict.extend(row[0])
 
-    #     print (dupePrintLst)
-    #     print (duplicateLst)
 
-    if len(duplicateLst) >= 1:
-        sql = '"OBJECTID" IN ({})'.format(', '.join(str(d) for d in duplicateLst))
-        duplicatePts_FL = arcpy.MakeFeatureLayer_management(inFc, 'duplicatePts_FL', sql)
-        print ('Deleted {} records'.format(len(duplicateLst)))
-        arcpy.DeleteFeatures_management(duplicatePts_FL)
-    else:
-        print ('No Duplicates found')
+
+    def get_report(duplicateDict):
+        return duplicateDict
+
+
+    def try_fix(self, duplicateDict):
+        try:
+            sql = '"OBJECTID" IN ({})'.format(', '.join(str(d) for d in duplicateDict['Duplicate Features']))
+            duplicate_FL = arcpy.MakeFeatureLayer_management(fc, 'duplicate_FL', sql)
+            print ('Deleted {} records'.format(len(duplicateLst)))
+            arcpy.DeleteFeatures_management(duplicatePts_FL)
+
     
-fc = r'C:\ZBECK\Addressing\Beaver\BeaverCounty.gdb\Address_ptsOLD'
-deleteDuplicateFeatures(fc)
+
