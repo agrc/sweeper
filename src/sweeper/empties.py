d78f122ebd02eaaaf3722b1293befe8549c703b7
Erik Neemann <38330621+eneemann@users.noreply.github.com>
2019-08-27 09:52:55 -0600

    cleaned up code, added logic to skip deletions when dict is empty

--- src/sweeper/empties.py
+++ src/sweeper/empties.py
@@ -10,6 +10,7 @@ import time
 import arcpy
 from arcpy import env
 import pprint
+import os
 
 # Start timer and print start time in UTC
 start_time = time.time()
@@ -27,56 +28,58 @@ fclist = arcpy.ListFeatureClasses()
 
 class EmptyTest(object):
     def __init__(self):
+        # Create dictionaries for reporting that are instance attributes
+        self.report = {}
+        self.final_report = {}
         pass
     
-    def delete_empty_geom(self, lyr):
-        del_count = 0
-        fields = ['OID@', 'Shape', 'SHAPE@']  # for point, polylines, or polygons
-        with arcpy.da.UpdateCursor(lyr, fields) as Ucursor:
-            print("Looping through rows in '{}' ...".format(lyr))
-            for row in Ucursor:
-                bad_geom = False
-                # Check if geometry object is null
-                if row[2] is None:
-                    print("     OID {} has null (None) geometry".format(row[0]))
-                    bad_geom = True
-                # Check shape centroid has a null coordinate
-                elif row[1][0] == None or row[1][1] == None:
-                    print("     OID {} has empty geometry".format(row[0]))
-                    bad_geom = True
-                
-                if bad_geom == True:
-                    print("     --> Deleting OID {} ... ".format(row[0]))
-                    Ucursor.deleteRow()
-                    del_count += 1
-        print("Total number of rows deleted: {}".format(del_count))
     
-    
-    def report_empty_geom(self, lyr):
+    def sweep(self, lyr):
         empty_count = 0
-        report_dict = {}
         fields = ['OID@', 'Shape', 'SHAPE@']  # for point, polylines, or polygons
         with arcpy.da.SearchCursor(lyr, fields) as Scursor:
             print("Looping through rows in '{}' ...".format(lyr))
             for row in Scursor:
                 bad_geom = False
+                
                 # Check if geometry object is null
                 if row[2] is None:
                     print("     OID {} has null (None) geometry".format(row[0]))
                     bad_geom = True
-                    report_dict[row[0]] = {'problem':'null geometry', 'action':'delete'}
+                    self.report[row[0]] = {'problem':'null geometry', 'action':'delete'}
+                    
                 # Check shape centroid has a null coordinate
                 elif row[1][0] == None or row[1][1] == None:
                     print("     OID {} has empty geometry".format(row[0]))
                     bad_geom = True
-                    report_dict[row[0]] = {'problem':'empty geometry', 'action':'delete or repair'}
+                    self.report[row[0]] = {'problem':'empty geometry', 'action':'delete or repair'}
+                    
                 if bad_geom == True:
-    #                print("     --> Deleting OID {} ... ".format(row[0]))
                     empty_count += 1
         print("Total number of empty geometries: {}".format(empty_count))
-        report = {'empties': report_dict}
-        pprint.pprint(report)
-        return report
+        self.final_report = {'empties': self.report}
+        
+    
+    def get_report(self):
+#        pprint.pprint(self.final_report)
+        return self.final_report
+    
+    
+    def try_fix(self, lyr):
+        if len(self.report) > 0:
+            del_count = 0
+            fields = ['OID@', 'Shape', 'SHAPE@']  # for point, polylines, or polygons
+            query = 'OBJECTID IN ({})'.format(', '.join(str(d) for d in self.report))
+            print(query)
+            with arcpy.da.UpdateCursor(lyr, fields, query) as Ucursor:
+                print("Looping through rows in '{}' ...".format(lyr))
+                for row in Ucursor:
+                    if row[0] in self.report:       # this is a redundant check that OID is in the dictionary
+                        Ucursor.deleteRow()
+                        del_count += 1
+            print("Total number of rows deleted: {}".format(del_count))
+        else:
+            pass
 
 
 ##########################
@@ -85,8 +88,12 @@ class EmptyTest(object):
 
 if __name__ == '__main__':
     for fc in sorted(fclist):
+        fc_path = os.path.join(database, fc)
         empty = EmptyTest()
-        report = empty.report_empty_geom(fc)
+        empty.sweep(fc_path)
+        empty.get_report()
+        empty.try_fix(fc_path)
+
 
 print('Script shutting down ...')
 # Stop timer and print end time in UTC
