3019d6b29ed3d5ec828a79b637125f4f6fdd0bcf
Greg Bunce <gbunce@utah.gov>
2019-09-13 16:25:03 -0600

    add backup option

--- src/sweeper/__main__.py
+++ src/sweeper/__main__.py
@@ -9,7 +9,7 @@ Usage:
   sweeper sweep invalids <table_name> --workspace=<workspace> [--verbose --try-fix --save-report=<report_path> --backup-to=<backup_path>]
 
 Arguments:
-  workspace   - path to feature class eg: `c:\\my.gdb`
+  workspace   - path to workspace eg: `c:\\my.gdb`
   table_name  - name of feature class or table eg: `Roads`
   report_path - folder to save report to eg: `c:\\temp`
   backup_path - place to create a temp gdb and import original table
@@ -20,13 +20,17 @@ Examples:
 import sys
 from docopt import docopt
 from .sweepers.empties import EmptyTest
-import report
+from . import report, backup
 
 def main():
     '''Main entry point for program. Parse arguments and pass to sweeper modules
     '''
 
     args = docopt(__doc__, version='1.0.0')
+
+    # backup file before quality checks
+    if args['--backup-to']:
+        backup.backup_data(args['--workspace'] + '\\' + args['<table_name>'], args['--backup-to'])
 
     if args['duplicates']:
         #: do something
@@ -37,12 +41,12 @@ def main():
         print(args)
     elif args['empties']:
         print(args)
-
+        # initialize the class object
         empty_sweeper = EmptyTest(args['--workspace'], args['<table_name>'])
-
         report_data = empty_sweeper.sweep()
-        if args['<report_path>']:
-            report.save_report(report_data, 'empties', args['<table_name>'], args['<report_path>'])
+
+        if args['--save-report']:
+            report.save_report(report_data, 'empties', args['<table_name>'], args['--save-report'])
         else:
             report.print_report(report_data, 'empties', args['<table_name>'])
 
--- src/sweeper/backup.py
+++ src/sweeper/backup.py
@@ -4,3 +4,19 @@
 backup.py
 A module that creates a gdb if it doesn't exist and inserts a feature class
 '''
+import arcpy 
+
+def backup_data(source_data, out_path):
+    # if the gdb does not exist, then create it
+    if not arcpy.Exists(out_path):
+        out_dir = out_path.rsplit('\\', 1)[0]
+        gdb_name = out_path.rsplit('\\', 1)[1]
+        arcpy.CreateFileGDB_management(out_dir, gdb_name)
+    
+    # get source_data feature class name
+    fc_name = source_data.rsplit('\\',1)[1]
+
+    # backup the source feature class
+    arcpy.Copy_management(source_data, out_path + '\\' + fc_name)
+
+
