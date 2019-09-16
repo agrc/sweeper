a1a6ee36cbf56f6a0ec7dd8b5cbf1a81e8607131
Zach Beck <zbeck@utah.gov>
2019-09-16 14:33:11 -0600

    misc updates hookup to cli

--- src/sweeper/sweepers/duplicates.py
+++ src/sweeper/sweepers/duplicates.py
@@ -9,50 +9,49 @@ import arcpy
 import re
 from xxhash import xxh64
 
-
 class DuplicateTest(object):
     def __init__(self):
+        self.report = {}
+        self.workspace = workspace
+        self.table_name = table_name
 
 
-    def sweep(self, fc):
+    def sweep(self):
 
         digestDict = {}
-        duplicateDict = {}
-        dupePrintLst = []
+
         digTrim = re.compile(r'(\d+\.\d{2})(\d+)')
 
         shps = ['Shape', 'SHAPE']
-
-        fields = [f.name for f in arcpy.ListFields(fc) if f.name not in shps]
+        fields = [f.name for f in arcpy.ListFields(self.table_name) if f.name not in shps]
         fields.append('SHAPE@WKT')
 
-        with arcpy.da.SearchCursor(fc, fields) as sCursor:
+        with arcpy.da.SearchCursor(self.table_name, fields) as sCursor:
             for row in sCursor:
                 shp = row[-1]
                 if shp != None:
                     coordTrim = digTrim.sub(r'\1', shp)
-                    hash = xxh64('{} {}'.format(row[1:], coordTrim))
+                    hash = xxh64('{} {}'.format(row[1:-1], coordTrim))
                     digest = hash.hexdigest()
                     if digest not in digestDict:
                         digestDict.setdefault(digest)
                     else:
-                        duplicates = duplicateDict.setdefault('Duplicate Features', [])
-                        duplicates.extend(row[0])
+                        self.report[row[0]] = 'duplicate feature'
+
                 else:
-                    duplicateDict.extend(row[0])
+                    self.report[row[0]] = 'empty geometry'
+
+        return self.report
 
 
-
-    def get_report(duplicateDict):
-        return duplicateDict
-
-
-    def try_fix(self, duplicateDict):
-        try:
-            sql = '"OBJECTID" IN ({})'.format(', '.join(str(d) for d in duplicateDict['Duplicate Features']))
-            duplicate_FL = arcpy.MakeFeatureLayer_management(fc, 'duplicate_FL', sql)
-            print ('Deleted {} records'.format(len(duplicateLst)))
-            arcpy.DeleteFeatures_management(duplicatePts_FL)
+    def try_fix(self):
+        if len(self.report) > 0:
+            try:
+                sql = '"OBJECTID" IN ({})'.format(', '.join(str(d) for d in self.report))
+                duplicate_FL = arcpy.MakeFeatureLayer_management(fc, 'duplicate_FL', sql)
+                print('Deleted {} duplicate records'.format(len(self.report)))
+                arcpy.DeleteFeatures_management(duplicate_FL)
+            except:
+                print('unable to delete features')
 
     
-
