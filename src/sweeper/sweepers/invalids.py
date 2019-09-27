9d497b52844352adad87f45c808454da878b615e
steveoh <sgourley@utah.gov>
2019-09-26 18:58:53 -0600

    chore: stub out invalids

--- src/sweeper/sweepers/invalids.py
+++ src/sweeper/sweepers/invalids.py
@@ -2,4 +2,20 @@
 # * coding: utf8 *
 
 
-    pass
+class InvalidSweeper():
+    '''A class that identifies invalid geometry objects and returns a report dictionary
+    '''
+    def __init__(self, workspace, table_name):
+        self.report = {}
+        self.workspace = workspace
+        self.table_name = table_name
+
+
+    def sweep(self):
+        '''A method to find invalid geometries and return a report dictionary
+        '''
+
+
+    def try_fix(self):
+        '''A method to that attempts to repair records with invalid geometries
+        '''
