1e2b94e5e56132763e1848c3dfaeeee7baf43f9f
Scott Davis <stdavis@utah.gov>
2020-04-03 08:49:54 -0600

    feat[metadata]: implement casing check for tags

--- src/sweeper/tests/test_metadata.py
+++ src/sweeper/tests/test_metadata.py
@@ -7,17 +7,35 @@ a module that tests the metadata sweeper
 from os import path
 from unittest import TestCase
 
-from ..sweepers.metadata import MetadataTest
-
+from ..sweepers.metadata import MetadataTest, title_case_tag
 
 current_folder = path.dirname(__file__)
 workspace = path.join(current_folder, 'data', 'Sweeper as CADASTRE.sde')
 
 
 class TestMetadataSweeper(TestCase):
+
     def test_no_tags(self):
         tool = MetadataTest(workspace, 'Sweeper.CADASTRE.MissingTags')
         report = tool.sweep()
 
         assert len(report['issues']) == 1
         self.assertRegex(report['issues'][0], r'missing tags')
+
+    def test_title_cased_tags(self):
+        tool = MetadataTest(workspace, 'Sweeper.CADASTRE.IncorrectCasing')
+        report = tool.sweep()
+
+        assert len(report['issues']) == 3
+        self.assertRegex(report['issues'][0], r'incorrectly cased tag')
+
+
+class TestTitleCaseTag(TestCase):
+
+    def test_title_case_tag(self):
+        #: input, expected
+        tests = [['agrc', 'AGRC'], ['Plss Fabric', 'PLSS Fabric'], ['water-related', 'Water-Related'], ['test', 'Test'],
+                 ['article is a good test', 'Article is a Good Test']]
+
+        for input_tag, expected in tests:
+            assert title_case_tag(input_tag) == expected
