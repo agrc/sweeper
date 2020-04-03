02930071e4a065d070339b8f114e3b761b260603
Scott Davis <stdavis@utah.gov>
2020-04-03 08:49:54 -0600

    feat[metadata]: implement casing check for tags

--- src/sweeper/sweepers/metadata.py
+++ src/sweeper/sweepers/metadata.py
@@ -10,6 +10,42 @@ from arcpy import metadata as md
 from arcpy import EnvManager, ListFeatureClasses, ListTables
 
 
+#: these constants were copied from https://github.com/agrc/agol-validator/blob/master/validate.py
+#: Tags or words that should be uppercased, saved as lower to check against
+UPPERCASED_TAGS = ['2g', '3g', '4g', 'agol', 'agrc', 'aog', 'at&t', 'blm', 'brat', 'caf', 'cdl', 'daq', 'dfcm', 'dfirm', 'dwq', 'e911', 'ems', 'fae', 'fcc', 'fema', 'gcdb', 'gis', 'gnis', 'hava', 'huc', 'lir', 'lrs', 'lte', 'luca', 'mrrc', 'nca', 'ng911', 'nox', 'npsbn', 'ntia', 'nwi', 'plss', 'pm10', 'psap', 'sao', 'sbdc', 'sbi', 'sgid', 'sitla', 'sligp', 'trax', 'uca', 'udot', 'ugs', 'uhp', 'uic', 'us', 'usao', 'usdw', 'usfs', 'usfws', 'usps', 'ustc', 'ut', 'uta', 'vcp', 'vista', 'voc']
+#: Articles that should be left lowercase.
+ARTICLES = ['a', 'the', 'of', 'is', 'in']
+
+
+#: copied from https://github.com/agrc/agol-validator/blob/master/checks.py with minor modifications
+def title_case_tag(tag):
+    '''
+    Changes a tag to the correct title case while also removing any periods:
+    'U.S. bureau Of Geoinformation' -> 'US Bureau of Geoinformation'. Should
+    properly upper-case any words or single tags that are acronyms:
+    'agrc' -> 'AGRC', 'Plss Fabric' -> 'PLSS Fabric'. Any words separated by
+    a hyphen will also be title-cased: 'water-related' -> 'Water-Related'.
+    Note: No check is done for articles at the begining of a tag; all articles
+    will be lowercased.
+    tag:        The single or multi-word tag to check
+    '''
+
+    new_words = []
+    for word in tag.split():
+        cleaned_word = word.replace('.', '').lower()
+
+        #: Upper case specified words:
+        if cleaned_word.lower() in UPPERCASED_TAGS:
+            new_words.append(cleaned_word.upper())
+        #: Lower case articles/conjunctions
+        elif cleaned_word.lower() in ARTICLES:
+            new_words.append(cleaned_word.lower())
+        #: Title case everything else
+        else:
+            new_words.append(cleaned_word.title())
+
+    return ' '.join(new_words)
+
 class MetadataTest():
     '''A class that validates geodatabase metadata
     '''
@@ -38,6 +74,13 @@ class MetadataTest():
 
         if len(missing_tags):
             report['issues'].append(f'missing tags: {required_tags}')
+
+        if metadata.tags is not None:
+            #: check casing for existing tags
+            for tag in tags:
+                formatted_tag = title_case_tag(tag)
+                if tag != formatted_tag:
+                    report['issues'].append(f'incorrectly cased tag: {tag} (should be: {formatted_tag})')
 
         return report
 
