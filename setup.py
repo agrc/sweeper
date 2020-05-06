c6e446404b1fb14927a97e1a02e654421564d3e4
Scott Davis <stdavis@utah.gov>
2020-05-05 18:34:37 -0600

    chore: v1.0.6 version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*names, **kwargs):
 
 setup(
     name="agrc-sweeper",
-    version="1.0.5",
+    version="1.0.6",
     license="MIT",
     description="CLI tool for making good data",
     long_description="",
