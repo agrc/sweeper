a1f3e1dfad9834f285db9893d21134700bb76c9e
Erik Neemann <eneemann@utah.gov>
2021-06-17 16:59:03 -0600

    add: bat file for eventual scheduled task

---
+++ scheduled_sweeper.bat
@@ -0,0 +1,2 @@
+call activate sweeper
+sweeper sweep --workspace=C:\Users\eneemann\AppData\Roaming\ESRI\ArcGISPro\Favorites\internal@SGID@internal.agrc.utah.gov.sde --change-detect --scheduled --save-report=C:\Temp
