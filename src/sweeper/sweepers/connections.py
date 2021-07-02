d85437185ab3d3685285b58852173f0e11107147
Erik Neemann <eneemann@utah.gov>
2021-07-02 13:11:54 -0600

    add: dictionary for database connections

---
+++ src/sweeper/sweepers/connections.py
@@ -0,0 +1,28 @@
+"""
+connections.py: A dictionary that points to necessary connection files
+"""
+
+dictionary = {
+    'BIOSCIENCE' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Bioscience.sde',
+    'BOUNDARIES' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Boundaries.sde',
+    'CADASTRE' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Cadastre.sde',
+    'CLIMATE' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Climate.sde',
+    'DEMOGRAPHICS' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Demographics.sde',
+    'ECONOMY' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Economy.sde',
+    'ELEVATION' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Elevation.sde',
+    'ENERGY' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Energy.sde',
+    'ENVIRONMENT' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Environment.sde',
+    'FARMING' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Farming.sde',
+    'GEOSCIENCE' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Geoscience.sde',
+    'HEALTH' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Health.sde',
+    'HISTORY' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_History.sde',
+    'INDICES' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Indices.sde',
+    'LOCATION' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Location.sde',
+    'PLANNING' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Planning.sde',
+    'POLITICAL' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Political.sde',
+    'RECREATION' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Recreation.sde',
+    'SOCIETY' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Society.sde',
+    'TRANSPORTATION' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Transportation.sde',
+    'UTILITIES' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Utilities.sde',
+    'WATER' : r'\\itwfpcap2\AGRC\sgid_to_agol\ConnectionFilesSGID\SGID_internal\SGID_Water.sde'
+}
