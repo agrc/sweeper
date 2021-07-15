ea62f246e5fb9d75bc55523362fca78233a4ee80
Erik Neemann <eneemann@utah.gov>
2021-07-15 11:23:34 -0600

    feat: apply relevant variables

--- src/sweeper/credentials_template.py
+++ src/sweeper/credentials_template.py
@@ -4,15 +4,9 @@ credentials_template.py: An example template of all the credentials and settings
 
 import socket
 
-ORG = ''  #: URL to AGOL organization (https://www.arcgis.com)
-USERNAME = ''  #: User whose items will be audited
-PASSWORD = ''  #: USERNAME's password
 DB = ''  #: Full path to sde connection file
-METATABLE = ''  #: Full path to SGID.META.AGOLItems metatable
-AGOL_TABLE = ''  #: URL for Feature Service REST endpoint for AGOL-hosted metatable
+CHANGE_DETECTION = #: Change detection table name with 'SGID.' prefix
 REPORT_BASE_PATH = ''  #: File path for report CSVs of everything that was fixed; rotated on each run
-LOG_ROTATE_COUNT = 90  #: Number of logs to rotate through (the n+1 oldest log will be deleted at rotate)
-CACHE_MAX_AGE = None  #: Number of seconds for the Cache Control/cacheMaxAge property (int)
 EMAIL_SETTINGS = {  #: Settings for EmailHandler
     'smtpServer': '',
     'smtpPort': 25,
